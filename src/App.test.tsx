a1c3145242dc88a92978559e180fc6c2066db700
tim <eps.timothy.vaughan@gmail.com>
2020-02-28 15:37:19 -0800

    Add statistical analysis

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,77 @@
-import { SimulateCombat } from './combatBalance'
+import { SimulateCombat, CombatScenario, CombatStats } from './combatBalance'
+
+const cTrials = 10000;
+
+interface Statistic {
+  mean : number,
+  sd: number
+}
+
+interface ScenarioReport{
+  playerWinRate : number,
+  playerInjuryRate : number,
+  avgRoundCount : Statistic,
+  avgActionCount : Statistic,
+  avgEnemyActionCount: Statistic
+}
 
 test('4 Players, Similar Numbers, Medium Difficulty', () => {
-  console.log(SimulateCombat(
-    { cNormal: 0, cDangerous: 2, cTough: 0, cScary: 0 },
-    { cNormal: 0, cDangerous: 2, cTough: 0, cScary: 0 },
-    { rgpicac: [
+  let scenario : CombatScenario = {
+    enemySetPrimary : { cNormal: 0, cDangerous: 0, cTough: 2, cScary: 0 },
+    enemySetSecondary : { cNormal: 0, cDangerous: 2, cTough: 0, cScary: 0 },
+    playerSet : {
+      rgpicac: [
         [2, 1, 1, 0, 0],
         [0, 2, 1, 1, 0],
         [0, 0, 2, 1, 1],
         [1, 0, 0, 2, 1],
       ]
-    }, true, 12));
+    },
+    isAmbush : false,
+    startingFocus : 12
+  }
+  let rgstats : CombatStats[] = [];
+  for(let i = 0; i < cTrials; i++)
+    rgstats.push(SimulateCombat(scenario));
+
+  let cPlayersWin = 0;
+  let cPlayerInjury = 0;
+  let totalRounds = 0;
+  let totalEnemyActions = 0;
+  let totalActions = 0;
+  rgstats.forEach((stats) => {
+    if(stats.didPlayersWin)
+      cPlayersWin++;
+    
+    if(stats.lowestPlayerHealth <= 5)
+      cPlayerInjury++;
+    
+    totalRounds += stats.cRound;
+    totalEnemyActions += stats.actionsEnemy;
+    totalActions += stats.actionsTotal;
+  });
+
+  let avgRoundCountMean = totalRounds / cTrials;
+  let avgActionCountMean = totalActions / cTrials;
+  let avgEnemyActionCountMean = totalEnemyActions / cTrials;
+  let avgRoundCountVarianceSquared = 0;
+  let avgActionCountVarianceSquared = 0;
+  let avgEnemyActionCountVarianceSquared = 0;
+  rgstats.forEach((stats) => {
+    // take each number, subtract the mean, square the result
+    // sum these differences
+    avgRoundCountVarianceSquared += Math.pow(stats.cRound - avgRoundCountMean, 2);
+    avgActionCountVarianceSquared += Math.pow(stats.actionsTotal - avgActionCountMean, 2);
+    avgEnemyActionCountVarianceSquared += Math.pow(stats.actionsEnemy - avgEnemyActionCountMean, 2);
+  })
+
+  let report : ScenarioReport = {
+    playerWinRate : cPlayersWin / cTrials,
+    playerInjuryRate : cPlayerInjury / cTrials,
+    avgRoundCount : { mean: avgRoundCountMean, sd: Math.sqrt(avgRoundCountVarianceSquared / cTrials) },
+    avgActionCount : { mean: avgActionCountMean, sd: Math.sqrt(avgActionCountVarianceSquared / cTrials) },
+    avgEnemyActionCount: { mean: avgEnemyActionCountMean, sd: Math.sqrt(avgEnemyActionCountVarianceSquared / cTrials) },
+  }
+  
+  console.log(report);
 });
--- src/combatBalance.ts
+++ src/combatBalance.ts
@@ -5,23 +5,42 @@ import { Player, PlayerSet, rgplayerFromPlayerSet } from './player'
 const playerActions = 2;
 const cRoundLimit = 10;
 
+export interface CombatScenario {
+  enemySetPrimary : EnemySet,
+  enemySetSecondary : EnemySet,
+  playerSet : PlayerSet,
+  isAmbush : boolean,
+  startingFocus : number
+}
+
+// did players win; players average health/focus, lowest health/focus; unspent tokens of each type; total actions, enemy actions
+export interface CombatStats {
+  didPlayersWin : boolean;
+  avgPlayerHealth : number;
+  avgPlayerFocus : number;
+  lowestPlayerHealth : number;
+  lowestPlayerFocus : number;
+  unspentAdv : number;
+  unspentDisadv : number;
+  unspentDef : number;
+  unspentExp : number;
+  actionsTotal : number;
+  actionsEnemy : number;
+  cRound: number;
+}
+
 // Given number of each type of enemy, number of players, assumed focus percentage, and who goes first
 // Return number of total actions and number of threat actions
-export function SimulateCombat(
-  enemySetPrimary : EnemySet,
-  enemySetSecondary : EnemySet, 
-  playerSet : PlayerSet, 
-  fPlayersFirst : boolean,
-  startingFocus : number)
+export function SimulateCombat(scenario : CombatScenario) : CombatStats
 {
   let actTotal = 0;
   let actEnemy = 0;
-  let rgenemyPrimary = rgEnemyFromEnemySet(enemySetPrimary);
-  let rgenemySecondary = rgEnemyFromEnemySet(enemySetSecondary);
-  let rgplayer = rgplayerFromPlayerSet(playerSet, playerActions, startingFocus);
+  let rgenemyPrimary = rgEnemyFromEnemySet(scenario.enemySetPrimary);
+  let rgenemySecondary = rgEnemyFromEnemySet(scenario.enemySetSecondary);
+  let rgplayer = rgplayerFromPlayerSet(scenario.playerSet, playerActions, scenario.startingFocus);
   
   // Combat
-  if(!fPlayersFirst)
+  if(scenario.isAmbush)
   {
     // 1 enemy action per player (how to decide what these are?)
     actEnemy += rgplayer.length;
@@ -32,6 +51,7 @@ export function SimulateCombat(
   let cRound = 0;
   while(!isPrimaryDefeated && !arePlayersDefeated && cRound < cRoundLimit)
   {
+    cRound++;
     arePlayersDefeated = true;
     rgplayer.forEach((player : Player) => {
       if(!player.isDead()) {
@@ -59,8 +79,65 @@ export function SimulateCombat(
         actTotal += actions;
       }
     });
-    cRound++;
   }
-  
-  return [actTotal, actEnemy];
+
+  return getCombatStats(!arePlayersDefeated && isPrimaryDefeated, rgplayer, rgenemyPrimary, rgenemySecondary, actTotal, actEnemy, cRound);
+}
+
+function getCombatStats(
+  didPlayersWin : boolean,
+  rgplayer : Player[],
+  rgenemyPrimary : Enemy[],
+  rgenemySecondary : Enemy[],
+  actionsTotal : number,
+  actionsEnemy : number,
+  cRound : number) : CombatStats
+{
+  let totalPlayerHealth = 0;
+  let totalPlayerFocus = 0;
+  let unspentAdv = 0;
+  let unspentDisadv = 0;
+  let unspentDef = 0;
+  let unspentExp = 0;
+  let lowestPlayerHealth = 15;
+  let lowestPlayerFocus = 12;
+  rgplayer.forEach((player : Player) => {
+    lowestPlayerHealth = Math.min(lowestPlayerHealth, player.health);
+    lowestPlayerFocus = Math.min(lowestPlayerFocus, player.focus);
+    totalPlayerHealth += player.health;
+    totalPlayerFocus += player.focus;
+    unspentAdv += player.adv;
+    unspentDisadv += player.disadv;
+    unspentDef += player.def;
+    unspentExp += player.exp;
+  });
+
+  rgenemyPrimary.forEach((enemy : Enemy) => {
+    unspentAdv += enemy.adv;
+    unspentDisadv += enemy.disadv;
+    unspentDef += enemy.def;
+    unspentExp += enemy.exp;
+  });
+
+  rgenemySecondary.forEach((enemy : Enemy) => {
+    unspentAdv += enemy.adv;
+    unspentDisadv += enemy.disadv;
+    unspentDef += enemy.def;
+    unspentExp += enemy.exp;
+  });
+
+  return {
+    didPlayersWin : didPlayersWin,
+    avgPlayerHealth : totalPlayerHealth / rgplayer.length,
+    avgPlayerFocus : totalPlayerFocus / rgplayer.length,
+    lowestPlayerHealth : lowestPlayerHealth,
+    lowestPlayerFocus : lowestPlayerFocus,
+    unspentAdv : unspentAdv,
+    unspentDisadv : unspentDisadv,
+    unspentDef : unspentDef,
+    unspentExp : unspentExp,
+    actionsTotal : actionsTotal,
+    actionsEnemy : actionsEnemy,
+    cRound : cRound
+  };
 }
