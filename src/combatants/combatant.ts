9658cd1c314eacb125b60fefb15932ca69c4c01b
Tim Vaughan (they/them) <eps.timothy.vaughan@gmail.com>
2020-06-19 18:13:52 -0700

    have combatants track index so can update them

--- src/combatants/combatant.ts
+++ src/combatants/combatant.ts
@@ -3,6 +3,7 @@ import { Token, Ability, Boost } from "../simulator/enum";
 export const initialTokens = [[0,0],[0,0]];
 
 export class Combatant {
+    index : number;
     health: number;
     actions: number;
     tokens : number[][];
@@ -10,7 +11,8 @@ export class Combatant {
     actionsTaken : number;
     isCritical : boolean;
 
-    constructor(health : number, actions : number, tokens : number[][], zone: number, actionsTaken : number, isCritical : boolean) {
+    constructor(index: number, health : number, actions : number, tokens : number[][], zone: number, actionsTaken : number, isCritical : boolean) {
+        this.index = index;
         this.health = health;
         this.actions = actions;
         this.tokens = tokens;
--- src/combatants/player.ts
+++ src/combatants/player.ts
@@ -6,11 +6,9 @@ export class Player extends Combatant {
     abilityScores : number[];
     focus: number;
 
-    constructor(health : number, actions: number, tokens : number[][], zone : number, actionsTaken : number, picac : number[], focus: number) {
-        super(health, actions, tokens, zone, actionsTaken, true /* players are always critical */);
+    constructor(index : number, health : number, actions: number, tokens : number[][], zone : number, actionsTaken : number, picac : number[], focus: number) {
+        super(index, health, actions, tokens, zone, actionsTaken, true /* players are always critical */);
         this.abilityScores = [...picac];
         this.focus = focus;
     }
 }
-
-export const TestPC : Player = new Player(initialPlayerHealth, 2, initialTokens, 0, 0, [1, 0, 1, 1, 1], 12);
