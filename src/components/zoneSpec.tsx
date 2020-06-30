abdd80e094545ef4ad4c9ffb8ec2489fdb84c9d0
Tim Vaughan (they/them) <eps.timothy.vaughan@gmail.com>
2020-06-30 16:37:33 -0700

    Implement ZoneSpec component

---
+++ src/components/zoneSpec.tsx
@@ -0,0 +1,105 @@
+import * as React from "react";
+import { EnemySet } from "../simulator/scenario";
+import { Terrain, TerrainOptions } from "../map/terrain";
+import { Card, Heading, Box, Flex } from "rebass";
+import { Label, Select, Input } from "@rebass/forms";
+
+interface ZoneSpecState {
+    zone : number;
+    // players (by index)
+    playerNames : string[];
+    // enemies
+    enemySet : EnemySet;
+    // terrain
+    terrain : Terrain;
+    // zones connected to
+    zonesConnectedTo : number[];
+}
+
+let badgeStyle = {
+    display: 'inline-block',
+    color: 'white',
+    bg: 'primary',
+    px: 2,
+    py: 1,
+    borderRadius: 9999,
+  };
+
+// zone index in top left
+// Terrain, right of that, as a drop-down selector
+// list of enemy types with combo box number input for each (Normal, Dangerous, Tough, Scary)
+// list of zones that are connected to as a tag list
+// list of players that are present as a tag list
+export class ZoneSpec extends React.Component<ZoneSpecState> {
+    render() {
+        return <Card
+            width={350}
+            sx={{p: 1, borderRadius: 2, boxShadow: '0 0 16px rgba(0, 0, 0, .25)'}}>
+                <Heading as='h3'>
+                    {'Zone ' + this.props.zone}
+                </Heading>
+                <Label>Terrain</Label>
+                <Select
+                    id='terrain'
+                    name='terrain'
+                    defaultValue={this.props.terrain.name}>
+                        {TerrainOptions.map((terrain, index) => (
+                        <option
+                            key={index}>
+                            {terrain.name}
+                        </option>))}
+                </Select>
+                <Heading as='h4'>Enemies</Heading>
+                <Flex mx={-2} mb={3}>
+                    <Box width={1/4} px={2}>
+                    <Label htmlFor='name'>Normal</Label>
+                    <Input
+                        id='normalEnemies'
+                        name='normalEnemies'
+                        type='number'
+                        min='0'
+                        defaultValue={this.props.enemySet.cNormal}
+                    />
+                    </Box>
+                    <Box width={1/4} px={2}>
+                    <Label htmlFor='name'>Dangerous</Label>
+                    <Input
+                        id='dangerousEnemies'
+                        name='dangerousEnemies'
+                        type='number'
+                        min='0'
+                        defaultValue={this.props.enemySet.cDangerous}
+                    />
+                    </Box>
+                    <Box width={1/4} px={2}>
+                    <Label htmlFor='name'>Tough</Label>
+                    <Input
+                        id='toughEnemies'
+                        name='toughEnemies'
+                        type='number'
+                        min='0'
+                        defaultValue={this.props.enemySet.cTough}
+                    />
+                    </Box>
+                    <Box width={1/4} px={2}>
+                    <Label htmlFor='name'>Scary</Label>
+                    <Input
+                        id='scaryEnemies'
+                        name='scaryEnemies'
+                        type='number'
+                        min='0'
+                        defaultValue={this.props.enemySet.cScary}
+                    />
+                    </Box>
+                </Flex>
+                <Heading as='h4'>Players</Heading>
+                {this.props.playerNames.map((playerName) => (
+                    <Box sx={badgeStyle}>{playerName}</Box>
+                ))}
+                <Heading as='h4'>Connected to</Heading>
+                {this.props.zonesConnectedTo.map((zone) => (
+                    <Box sx={badgeStyle}>{'Zone '+zone}</Box>
+                ))}
+        </Card>
+    }
+}
--- src/map/terrain.ts
+++ src/map/terrain.ts
@@ -1,38 +1,49 @@
 import { Attack } from "../simulator/enum";
 
 export class Terrain {
+    name : string;
     attackBoost : (attackType : Attack) => number;
     defenseBoost : (defenseType : Attack) => number;
 
     constructor(
+        name : string,
         attackBoost : (attackType : Attack) => number,
         defenseBoost : (defenseType : Attack) => number
     ) {
+        this.name = name;
         this.attackBoost = attackBoost;
         this.defenseBoost = defenseBoost;
     }
 }
 
 export const TerrainDefault = new Terrain(
+    "Default",
     (attackType) => 0,
     (defenseType) => 0);
 
 export const TerrainCover = new Terrain(
+    "Cover",
     (attackType) => 0,
     (defenseType) => defenseType == Attack.Ranged ? 1 : 0);
 
 export const TerrainExposed = new Terrain(
+    "Exposed",
     (attackType) => 0,
     (defenseType) => defenseType == Attack.Ranged ? -1 : 0);
 
 export const TerrainVantage = new Terrain(
+    "Vantage Point",
     (attackType) => attackType == Attack.Ranged ? 1 : 0,
     (defenseType) => 0);
 
 export const TerrainUnstable = new Terrain(
+    "Unstable",
     (attackType) => -1,
     (defenseType) => 0);
 
 export const TerrainUneven = new Terrain(
+    "Uneven",
     (attackType) => attackType == Attack.Melee ? -1 : 0,
     (defenseType) => 0);
+
+export const TerrainOptions = [ TerrainDefault, TerrainCover, TerrainExposed, TerrainVantage, TerrainUnstable, TerrainUneven ];
