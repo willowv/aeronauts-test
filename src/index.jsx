b8129cfecd3e447dd31a14eae0f81ff60b93e12e
Tim Vaughan (they/them) <eps.timothy.vaughan@gmail.com>
2020-06-30 16:36:28 -0700

    Use rebass

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import {ThemeProvider} from 'emotion-theming';
+import theme from '@rebass/preset'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
