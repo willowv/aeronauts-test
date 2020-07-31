3e2e97365a37d3122590a18248e7f34463cc6095
Tim Vaughan (they/them) <eps.timothy.vaughan@gmail.com>
2020-07-31 16:49:00 -0700

    Improve player AI

--- src/simulation/combatants/ai/playerAi.ts
+++ src/simulation/combatants/ai/playerAi.ts
@@ -1,5 +1,6 @@
 import { AI } from "./ai";
 import Combatant from "../combatant";
+import { Token, Boost } from "../../../enum";
 
 const minimum = (array: number[]) => Math.min(...array);
 const sum = (array: number[]) => array.reduce((x, y) => x + y, 0);
@@ -10,31 +11,46 @@ export const PlayerAI = new AI("Player", (state) => {
   let playerHealths = alivePlayers.map((player) => player.health);
 
   let playerHealthScore = 0;
-  playerHealthScore += sum(playerHealths); // 1 point for each point of health remaining
   playerHealthScore += minimum(playerHealths); // encourage distributing damage
 
   // enemy health and kills - make progress
+  let aliveEnemies = state.enemies.filter((enemy) => !enemy.isDead());
+  let deadEnemies = state.enemies.filter((enemy) => enemy.isDead());
   let enemyHealthScore = 0;
   enemyHealthScore += sum(
-    state.enemies.map((enemy) => enemy.maxHealth - Math.max(enemy.health, 0))
+    aliveEnemies.map((enemy) => enemy.maxHealth - enemy.health)
   );
   enemyHealthScore += sum(
-    state.enemies
-      .filter((enemy) => enemy.isDead())
-      .map((enemy) => enemy.actionsPerTurn) // encourage kills
+    deadEnemies.map((enemy) => enemy.maxHealth + enemy.actionsPerTurn) // encourage kills
   );
 
   // Return true if some action has at least one valid target
-  let canTarget = (player: Combatant) =>
+  let canTargetAny = (player: Combatant) =>
     player.actions.some((action) => {
       let targets = action.GetValidTargets(state, player);
       return targets.length > 0;
     });
-  // positioning - make sure you can target
+
+  let canTarget = (actor : Combatant, target : Combatant) =>
+    actor.actions.some((action) => {
+      let targets = action.GetValidTargets(state, actor);
+      return targets.some((target2) => target2 === target);
+    });
+
+  let threatOn = (player : Combatant) => {
+    return sum(aliveEnemies.map((enemy) => {
+      return canTarget(enemy, player) ? enemy.actionsPerTurn : 0;
+    }))
+  }
+
+  // Measures potential damage / harm from this position and token set up.
   let positioningScore = sum(
     alivePlayers.map((player) => {
-      // 1 point for each player that has valid targets
-      return canTarget(player) ? 1 : 0;
+      // 6 is based on two actions with expected value 3 (from external calculation)
+      let offenseScore = canTargetAny(player) ? 6 + player.tokens[Token.Action][Boost.Positive] - player.tokens[Token.Action][Boost.Negative] : 0;
+      // x2 is based on enemy actions having expected value 2 (from external calculation)
+      let defenseScore = player.health - threatOn(player) * 2 + player.tokens[Token.Defense][Boost.Positive] - player.tokens[Token.Defense][Boost.Negative];
+      return offenseScore + defenseScore;
     })
   );
   return playerHealthScore + enemyHealthScore + positioningScore;
