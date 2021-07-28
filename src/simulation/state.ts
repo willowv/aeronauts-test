cdd24e20b49dc6286dcb5c5037690e894a556bb6
Willow Vaughan (they/them) <eps.willow@gmail.com>
2021-07-27 18:28:07 -0700

    State support for airships

--- src/simulation/state.ts
+++ src/simulation/state.ts
@@ -1,17 +1,34 @@
 import { Player } from "./combatants/player";
 import Combatant from "./combatants/combatant";
 import { Faction } from "../enum";
+import { Airship } from "./airships/airship";
+import { PlayerAirship } from "./airships/playerAirship";
 
 export class CombatState {
   players: Player[];
+  playerAirship: PlayerAirship | null;
   enemies: Combatant[];
+  enemyAirship: Airship | null;
+  isAirCombat: boolean;
 
-  constructor(players: Player[], enemies: Combatant[]) {
+  constructor(
+    players: Player[],
+    playerAirship: PlayerAirship | null,
+    enemies: Combatant[],
+    enemyAirship: Airship | null,
+    isAirCombat: boolean
+  ) {
     this.players = players;
+    this.playerAirship = playerAirship;
     this.enemies = enemies;
+    this.enemyAirship = enemyAirship;
+    this.isAirCombat = isAirCombat;
   }
 
   ArePlayersDefeated(): boolean {
+    if (this.isAirCombat && this.playerAirship !== null)
+      return this.playerAirship.isDead();
+
     // All critical players are dead
     return this.players
       .filter((player) => player.isCritical)
@@ -19,6 +36,9 @@ export class CombatState {
   }
 
   AreEnemiesDefeated(): boolean {
+    if (this.isAirCombat && this.enemyAirship !== null)
+      return this.enemyAirship.isDead();
+
     // All critical enemies are dead
     return this.enemies
       .filter((enemy) => enemy.isCritical)
@@ -42,9 +62,27 @@ export class CombatState {
     return this.players[combatant.index];
   }
 
+  getPlayerCaptain(): Player | null {
+    if (this.playerAirship !== null)
+      return this.players[this.playerAirship.indexPlayerCaptain];
+    else return null;
+  }
+
+  getPlayerEngineer(): Player | null {
+    if (this.playerAirship !== null)
+      return this.players[this.playerAirship.indexPlayerEngineer];
+    else return null;
+  }
+
   clone(): CombatState {
     let clonePlayers = this.players.map((player) => player.clone());
     let cloneEnemies = this.enemies.map((combatant) => combatant.clone());
-    return new CombatState(clonePlayers, cloneEnemies);
+    return new CombatState(
+      clonePlayers,
+      this.playerAirship?.clone() ?? null,
+      cloneEnemies,
+      this.enemyAirship?.clone() ?? null,
+      this.isAirCombat
+    );
   }
 }
